1998750186d1c0f88935704895a10f21558c3e4d
Kamel <dr_ksaleh@yahoo.com>
2022-02-01 11:48:50 +0200

    added an error message for the python version

--- vintro.py
+++ vintro.py
@@ -1,11 +1,16 @@
 import platform
 from variables import Variables 
+from termcolor import colored
 
 # vintro stands for version and introduction
 
 class VIntro:
     def intro():
-        return f"""PMPL Version 3.0 Badlands for {platform.system()}
+        python = platform.python_version_tuple()
+        if int(python[1]) < 10:
+            print(colored("\nERROR: Upgrade to python version >3.10 to use the programming language!\nIf you got this message and you have version >3.10, send us a github issue!", "red")) # make sure the user has the required python version
+            quit()
+        return f"""PMPL Version 3.1 Badlands for {platform.system()}
 Python Interpreter Version {platform.python_version()}
 Press Ctrl+C to exit or type 'quit'
 """
